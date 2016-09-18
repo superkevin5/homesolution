0a26f9428eea1a8e85e45884f6a72d8ac8cc358c
superkevin5 <hlmloveaustralia@gmail.com>
2016-09-18 18:12:39 +1000

    finish crawler

--- rest/scraper/casperprototy.js
+++ rest/scraper/casperprototy.js
@@ -1,42 +1,126 @@
-var links = [];
-var util = require('util');
-var casper = require('casper').create({
-    verbose: false,
-    logLevel: "debug",
-    pageSettings: {
-        loadImages: false,
-// The WebPage instance used by Casper will
-        loadPlugins: false
-    }
-});
-
-////
-//casper.start('https://www.google.com.au/#gfe_rd=cr');
-////
-//casper.thenOpen('https://www.google.com.au');
-////
-////
-////casper.thenOpen('http://foo.bar/3');
-//
-//
-////casper.back();
-//
-//casper.run(function(){
-//    console.log('ssss');
-//    console.log(this.getCurrentUrl());
+//var links = [];
+//var util = require('util');
+//var casper = require('casper').create({
+//    verbose: false,
+//    logLevel: "debug",
+//    pageSettings: {
+//        loadImages: false,
+//// The WebPage instance used by Casper will
+//        loadPlugins: false
+//    }
 //});
+//
+//////
+////casper.start('https://www.google.com.au/#gfe_rd=cr');
+//////
+////casper.thenOpen('https://www.google.com.au');
+//////
+//////
+//////casper.thenOpen('http://foo.bar/3');
+////
+////
+//////casper.back();
+////
+////casper.run(function(){
+////    console.log('ssss');
+////    console.log(this.getCurrentUrl());
+////});
+//
+//casper.start();
+//casper.then(function() {
+//// This step will be executed
+//});
+//casper.then(function() {
+//    this.bypass(2);
+//});
+//casper.then(function() {
+//// This test won't be executed
+//});
+//casper.then(function() {
+//// Nor this one
+//});
+//casper.run();
 
-casper.start();
-casper.then(function() {
-// This step will be executed
-});
-casper.then(function() {
-    this.bypass(2);
-});
-casper.then(function() {
-// This test won't be executed
-});
-casper.then(function() {
-// Nor this one
-});
-casper.run();
+//
+//var Vehicle = (function(){
+//    function Vehicle(year,make,model){
+//        this.year = year;
+//        this.make = make;
+//        this.model = model;
+//    }
+//
+//
+//    Vehicle.prototype.getInfo = function(){
+//        return this.make + this.model;
+//    }
+//    return Vehicle;
+//})();
+//
+//
+//var Car =(function(parent){
+//
+//    Car.prototype = new Vehicle();
+//    Car.prototype.constructor = Car;
+//
+//    function Car(year, make, model) {
+//        parent.call(this,year,make,model);
+//        this.wheelQuantity = 4;
+//    }
+//    Car.prototype.getInfo = function() {
+//        return 'Vehicle Type: Car' + parent.prototype.getInfo.call(this);
+//    };
+//    return Car;
+//})(Vehicle);
+//
+//
+//
+//var Car1 = new Car(1989,2,3);
+//console.log(Car1.getInfo());
+//
+
+//var Animal = (function(){
+//    function Animal(name){
+//
+//        this.name = name;
+//    }
+//    Animal.prototype.say = function(){
+//      console.log(this.name);
+//    };
+//    return Animal;
+//})();
+//
+//
+//var Dog = (function(parent){
+//    Dog.prototype = new Animal();
+//    Dog.prototype.constructor = Dog;
+//
+//    function Dog(name,age){
+//        parent.call(this, name);
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    return Dog;
+//})(Animal);
+//
+//var d = new Dog("a",1);
+//console.log(d);
+//d.say();
+
+
+var Book = function(name){
+    this.name = name;
+};
+
+var parent = new Book('ss');
+
+
+
+var rbook = function(){
+    Book.call(this,'123');
+};
+
+rbook.prototype = Book.prototype;
+
+var c = new rbook();
+console.log(c.name);
--- rest/scraper/realestate.js
+++ rest/scraper/realestate.js
@@ -1,5 +1,96 @@
-var links = [];
+var propertyDetailsLinks = [];
+var propertyMap = {};
+var currentPage = 0;
 var util = require('util');
+
+var Property = (function () {
+    function Property(propertyId, DetailUrl, streetAddress, addressLocation, addressionRegion, postalCode, pageVisit) {
+        this.propertyId = propertyId,
+            this.DetailUrl = DetailUrl,
+            this.streetAddress = streetAddress,
+            this.addressLocation = addressLocation,
+            this.addressionRegion = addressionRegion,
+            this.postalCode = postalCode,
+            this.pageVisit = pageVisit
+    }
+
+    return Property;
+})();
+
+
+function getLinks(item) {
+    var links = document.querySelectorAll(item);
+    return Array.prototype.map.call(links, function (e) {
+        return e.getAttribute('href')
+    });
+};
+
+
+function printLinks(detailsLinks, IDlinks) {
+    for (var i = 0; i < detailsLinks.length; i++) {
+        console.log(detailsLinks[i], '  ', IDlinks[i], '\n');
+    }
+};
+
+function getInnerHtmlBySelector(selector) {
+    return document.querySelector(selector).innerHTML.trim();
+};
+
+function clearPropertyDetailsLinks(propertyDetailsLinks) {
+    propertyDetailsLinks = [];
+};
+
+function goToPage(currentPage) {
+    return casper.thenOpen('http://www.realestate.com.au/buy/in-2017/list-' + (currentPage));
+};
+
+function init() {
+
+};
+
+function run() {
+    casper.echo('Current page is ' + casper.getCurrentUrl(), 'INFO');
+
+    var isNoMatchExist = casper.evaluate(getInnerHtmlBySelector, '.noMatch');
+
+    if(isNoMatchExist) {
+        casper.echo('No more to crawl Done.').exit();
+    }
+
+    propertyDetailsLinks = casper.evaluate(getLinks, '.detailsButton');
+    var IDlinks = Array.prototype.map.call(propertyDetailsLinks, function (e) {
+        return e.trim().replace(/\?.*/g, '').match(/\d+$/g);
+    });
+    printLinks(propertyDetailsLinks, IDlinks);
+    queryPropertyDetail(propertyDetailsLinks);
+    clearPropertyDetailsLinks(propertyDetailsLinks);
+    goToPage(++currentPage).then(function (data) {
+        run();
+    });
+};
+
+function queryPropertyDetail(propertyDetailsLinks) {
+    var i = -1;
+    casper.each(propertyDetailsLinks, function () {
+        i++; // change the link being opened (has to be here specifically)
+        var pId = propertyDetailsLinks[i].trim().replace(/\?.*/g, '').match(/\d+$/g);
+        casper.thenOpen('http://www.realestate.com.au' + propertyDetailsLinks[i]).then(function () {
+
+            var detailUrl = propertyDetailsLinks[i];
+            var streetAddress = this.evaluate(getInnerHtmlBySelector, 'span[itemprop="streetAddress"]');
+            var addressLocality = this.evaluate(getInnerHtmlBySelector, 'span[itemprop="addressLocality"]');
+            var addressRegion = this.evaluate(getInnerHtmlBySelector, 'span[itemprop="addressRegion"]');
+            var postalCode = this.evaluate(getInnerHtmlBySelector, 'span[itemprop="postalCode"]');
+            var p = new Property(pId, detailUrl, streetAddress, addressLocality, addressRegion, postalCode, null);
+            propertyMap[pId] = p;
+        }).thenOpen('http://www.realestate.com.au/pdpvisits.ds?id=' + pId).then(function () {
+            var pageVisitsCount = this.evaluate(getInnerHtmlBySelector, '.page-visits-count');
+            propertyMap[pId].pageVisit = pageVisitsCount;
+            casper.echo('property ' + pId + ' stored into map', 'INFO');
+        });
+    });
+};
+
 var casper = require('casper').create({
     verbose: false,
     logLevel: "debug",
@@ -8,64 +99,42 @@ var casper = require('casper').create({
 // The WebPage instance used by Casper will
         loadPlugins: false
     }
-});
+}).userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X)');
 
 
 casper.start('https://www.realestate.com.au/buy', function () {
-    //document.querySelector('#where').value = "2017";
-
-    //document.getElementById('where').value = '2017';
-    //console.log('522');
-    //console.log(document.querySelector('#where'));
-    this.echo('Page title is: ' + this.evaluate(function () {
+    casper.echo('Page title is: ' + this.evaluate(function () {
             return document.title;
         }), 'INFO');
-
-    ////this.fillSelectors('.search-form', {
-    ////    'input[name="where"]': '2017',
-    ////    'input[name="propertyType"]': 'unit apartment',
-    ////    'input[name="numBeds"]': '2',
-    ////    'input[name="maxBeds"]': '2',
-    ////    'input[name="maxPrice"]': '1000000'
-    ////}, true);
-    //console.log('522');
-    //
-    //this.echo('Page title is: ' + this.evaluate(function () {
-    //        console.log('1');
-    //    }), 'INFO');
-    //
-    //
-    //this.evaluate(function() {
-    //    console.log('1');
-    //    //console.log(document.querySelector('input[name="where"]'));
-    //    console.log('5');
-    //});
-
-    //http://www.realestate.com.au/pdpvisits.ds?id=123417170&theme=rea.buy
-});
-
-
-casper.thenEvaluate(function (term) {
+}).thenEvaluate(function (term) {
     document.getElementById("where").value = '2017';
     document.getElementById("rui-property-type-select-id").value = "unit apartment";
     document.getElementById("rui-min-beds-select-id").value = 2;
     document.getElementById("rui-max-beds-select-id").value = 2;
-
-
     document.querySelector(".rui-search-button").click();
-}, 'Caspser');
+}, 'Caspser').waitForUrl(/.*2017.*/, function () {
+    casper.echo('clicked ok, new location is ' + this.getCurrentUrl(), 'INFO');
+    var currentUrl = this.getCurrentUrl();
+    var match = currentUrl.match(/.*list-(\d+).*/);
+    currentPage = match[1];
+    currentPage = 1000;
+});
 
 
-casper.waitForUrl(/.*2017.*/, function () {
-    this.echo('clicked ok, new location is ' + this.getCurrentUrl(), 'INFO');
+casper.then(function () {
+    run();
+});
 
 
+//http://www.realestate.com.au/property-apartment-nsw-waterloo-123369930
 
-    var links = this.evaluate(function(){
-        return getLinks();
-    });
+//http://www.realestate.com.au/buy/in-2017/list-1?activeSort=price-asc
+//page visit example
+//http://www.realestate.com.au/pdpvisits.ds?id=121801722
+//http://www.realestate.com.au/buy/in-2017/list-1
 
-    console.log(links);
+casper.on('remote.message', function (msg) {
+    //this.echo('remote message caught: ' + msg);
 });
 
 casper.on('url.changed', function (url) {
@@ -76,13 +145,3 @@ casper.run(function () {
     this.echo('Done.').exit();
 });
 
-
-function getLinks(className) {
-    var links = document.querySelectorAll('.detailsButton');
-    console.log('a');
-    console.log(links);
-    console.log(links.length);
-    return Array.prototype.map.call(links, function (e) {
-        return e.getAttribute('href')
-    });
-}
